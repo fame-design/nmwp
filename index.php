3a09f4e3cd84b64b1ef8c619cc2487c99ef759e4
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-06-28 21:56:33 +0200

    update structure

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php get_header(); ?>
 <div class="content-wrapper">
-<ul>
-    <?php if ( have_posts() ) :
-        while ( have_posts() ) : the_post(); ?>
-            <li>
-                <div class="postsListEntry">
-                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                    <div> <?php the_post_thumbnail(); ?> </div>
-                    <div> <?php the_excerpt(); ?> </div>
-                    <div>
-                        <div> <?php the_author(); ?> <?php the_date('Y-m-d'); ?></div>
-                        <div> <?php the_tags(); ?> <div>
-                        <div> categories: <?php echo get_the_category_list(' | '); ?> <div>
-                    </div>
-                </div>
-            </li>
-        <?php endwhile;
-    endif; ?>
-</ul>
+    <h2>latest posts</h2>
+    <ul class="postsList">
+        <?php if ( have_posts() ) :
+            while ( have_posts() ) : the_post(); ?>
+                <li class="postsListEntry">
+
+                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <div> <?php the_post_thumbnail(); ?> </div>
+                        <div> <?php the_excerpt(); ?> </div>
+                        <div>
+                            <span>by <?php the_author(); ?> <?php the_date('Y-m-d'); ?></span>
+                            <span> <?php the_tags(); ?> </span>
+                            <span> categories: <?php echo get_the_category_list(' | '); ?> </span>
+                        </div>
+
+                </li>
+            <?php endwhile;
+        endif; ?>
+    </ul>
 </div>
 <?php get_footer(); ?>
