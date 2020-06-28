06a6b9f206f06e3171f8d85f320aa59509467bba
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-06-28 16:52:42 +0200

    split css files

--- functions.php
+++ functions.php
@@ -2,7 +2,9 @@
 
 function load_scripts(){
     //wp_enqueue_style(handle, src, deps, ver, media);
-    wp_enqueue_style('stylesheet', get_template_directory_uri() . '/css/stylesheet.css', false, '0.0.1', 'all');
+    wp_enqueue_style( 'theme-colors', get_template_directory_uri() .'/css/colors.css', array(), null, 'all' );
+    wp_enqueue_style( 'layout', get_template_directory_uri() .'/css/layout.css', array(), null, 'all' );
+    
     //wp_enqueue_script(handle, src, deps, ver, in_footer);
     wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array(), false, true);
 }
