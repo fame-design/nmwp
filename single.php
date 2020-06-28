6f87bdf44d409d615e24ba8a2c1f3d7db51f2062
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-06-28 20:09:28 +0200

    add featured image to single.php

--- single.php
+++ single.php
@@ -3,6 +3,7 @@
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     
         <div class="single-post">
+            <div> <?php the_post_thumbnail(); ?> </div>
             <h2><?php the_title(); ?></h2>
             <div> <?php the_author(); ?> <?php the_date('Y-m-d'); ?></div>
             <div> <?php the_category(); ?> </div>
