b138554363c0dd94d6d113c8ad6bf4fa8eeceb1f
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-06-28 19:48:05 +0200

    update index styles

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <?php if ( have_posts() ) :
         while ( have_posts() ) : the_post(); ?>
             <li>
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                <div> <?php the_post_thumbnail(); ?>  </div>
-                <div><?php the_excerpt(); ?></div>
-                <div><?php comments_template(); ?></div>
+                <div class="postsListEntry">
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <div> <?php the_post_thumbnail(); ?> </div>
+                    <div> <?php the_excerpt(); ?> </div>
+                </div>
             </li>
         <?php endwhile;
     endif; ?>
