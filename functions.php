a14137b367753c9488e24272cdd25f5cecef796e
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-06-29 23:49:09 +0200

    register navbars

--- functions.php
+++ functions.php
@@ -8,24 +8,35 @@ function load_scripts(){
     //wp_enqueue_script(handle, src, deps, ver, in_footer);
     wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array(), false, true);
 }
-
 add_action('wp_enqueue_scripts', 'load_scripts');
 
 //activate featured images
 add_theme_support('post-thumbnails');
 
-
-
 //customize excerpt
 function wpdocs_custom_excerpt_length(){
     return 50;
 }
-
 add_filter('excerpt_length', 'wpdocs_custom_excerpt_length');
 
 function wpdocs_excerpt_more(){
     return '<a href=' . get_the_permalink() . '> Read More</a>';
 }
 add_filter('excerpt_more', 'wpdocs_excerpt_more');
+
+
+//navigations
+function load_navigation(){
+    register_nav_menus(array(
+        'header_navbar' => 'Header Navbar',
+        'footer_navbar' => 'Footer Navbar'
+    ));
+}
+
+add_action('after_setup_theme', 'load_navigation');
+
+
+
+
 
 ?>
